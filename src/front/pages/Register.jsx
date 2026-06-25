50467d10ce4107203228586787f09cdf3d6ebc6e
Kenube21 <kendallsh51@gmail.com>
2026-06-25 02:28:58 +0000

    register page

---
+++ src/front/pages/Register.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom"
+
+const Register = () => {
+
+    return (<div className="container">
+        <div className="card text-center mt-5">
+            <div className="card-header">
+                Register User
+            </div>
+            <div className="card-body">
+                <div class="mb-3">
+                    <label for="email" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="email" aria-describedby="emailHelp"/>
+                </div>
+                <div class="mb-3">
+                    <label for="password" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="password" aria-describedby="passwordHelp"/>
+                </div>
+                <div class="mb-3">
+                    <label for="user" className="form-label">User Name</label>
+                    <input type="text" className="form-control" id="user" aria-describedby="userHelp"/>
+                </div>
+                <Link to={""} className="btn btn-success">Register</Link>
+            </div>
+            <div className="card-footer text-body-secondary">
+                <Link to={"/loggin"}>Did you register???</Link>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default Register
